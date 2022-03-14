a20e841cd29ee4412a616913f2cfd765b5858807
Damiencqh <99311220+Damiencqh@users.noreply.github.com>
2022-03-14 12:04:48 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("NTutors Listings"),
+          title: Text("Modules Listings"),
           backgroundColor: Colors.greenAccent,
         ),
 
